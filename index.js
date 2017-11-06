026ee167f78b1b254ba14cc20da87e0cb6851919
Amy Jackson <aj81623@gmail.com>
2017-11-06 01:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Mon Ster';
 const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ';
+const shortGreeting = 'Welcome, ${currentUser}';
